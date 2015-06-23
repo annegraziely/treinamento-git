70230e1d8855e314ed87086b05a4086defa2b2e0
Tailan Mendes <tailan.mendes@iatecam.org.br>
2015-06-23 10:38:14 -0400

    Metodo pow

--- CalculadoraCientifica.java
+++ CalculadoraCientifica.java
@@ -1,3 +1,14 @@
 public class CalculadoraCientifica {
+
+	/** Method: pow 
+	    Author: Created by MENDES, T. C.
+	    param [in] base
+	    param [out] expo - expoente
+	*/
+
+	public double pow(double base, double expo)
+	{
+		return base^expo;
+	}
 	
 }
